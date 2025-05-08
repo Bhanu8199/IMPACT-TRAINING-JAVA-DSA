f3ab8b9e0912ee14069a1721f10df0b63f1e6c38
BHANUVARDHAN MEDAPALLI <160517143+Bhanu8199@users.noreply.github.com>
2025-05-08 09:23:53 +0530

    Add files via upload

---
+++ SWAPPING_2NUMBERS.java
@@ -0,0 +1,13 @@
+public class May8 {
+  public static void main(String[] args) {
+    
+    //SWAPPING OF TWO NUMBERS
+      int a = 10;
+      int b =20;
+      int temp = a;
+      a = b;
+      b = temp;
+      System.out.println(" " + a);
+      System.out.println(" " + b);
+  }  
+}
