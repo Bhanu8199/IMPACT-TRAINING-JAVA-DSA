e0fdb780ea0743fd6c29abdc6cbe69582cbe0fb8
BHANUVARDHAN MEDAPALLI <160517143+Bhanu8199@users.noreply.github.com>
2025-05-09 09:50:41 +0530

    Add files via upload

---
+++ REVERSE_STRING.java
@@ -0,0 +1,13 @@
+public class May9 {
+    public static void main(String[] args) {
+      
+      // REVERSE THE STRING
+      
+      String name="Parul University";
+      for(int i=name.length()-1;i>=0;i--){
+        System.out.print(name.charAt(i));
+        
+      }
+      
+  }    
+}
