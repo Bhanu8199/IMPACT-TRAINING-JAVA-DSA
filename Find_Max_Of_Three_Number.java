7585312f5df6516a6d5233edece5c87f260888be
BHANUVARDHAN MEDAPALLI <160517143+Bhanu8199@users.noreply.github.com>
2025-05-10 07:37:29 +0530

    Add files via upload

---
+++ Find_Max_Of_Three_Number.java
@@ -0,0 +1,17 @@
+public class May2 {
+  public static void main(String[] args){
+    // find max of three Number
+    int a=12;
+    int b=13;
+    int c=14;
+    
+    // conditional statement if and else
+    if(a<b && a>c){
+      System.out.println("A is largest");
+    }else if(b>c){
+      System.out.println("B is largest");
+    }else{
+      System.out.println("C is largest");
+    }
+  }
+}
