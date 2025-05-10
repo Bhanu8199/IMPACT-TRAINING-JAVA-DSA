6951c1d64d667e2ce590a4c43ac011896f7a27d0
BHANUVARDHAN MEDAPALLI <160517143+Bhanu8199@users.noreply.github.com>
2025-05-10 07:40:41 +0530

    Add files via upload

---
+++ Find_Max_Of_Four_Number.java
@@ -0,0 +1,21 @@
+public class May2 {
+  public static void main(String[] args){
+    
+    // find max of four Number(task)
+    int a=12;
+    int b=13;
+    int c=14;
+    int d=56;
+    
+    // conditional statement if and else
+    if(a<b && a>c && a>d){
+      System.out.println("A is largest");
+    }else if(b>c){
+      System.out.println("B is largest");
+    }else if(c>d){
+      System.out.println("C is largest");
+    }else{
+      System.out.println("D is largest");
+    }
+  }
+}
