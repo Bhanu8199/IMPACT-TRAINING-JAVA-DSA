717e79327a0c192ab4e742646c9590568f2d22d3
BHANUVARDHAN MEDAPALLI <160517143+Bhanu8199@users.noreply.github.com>
2025-05-10 07:34:58 +0530

    Add files via upload

---
+++ VARIABLES, DATATYPES, OPERATORS.java
@@ -0,0 +1,11 @@
+public class May2 {
+  public static void main(String[] args){
+    // // variables,(datatypes->int,long,float,char,double,boolean),(operators->+,-,/,*,%)
+    System.out.println(2%12);
+    System.out.println(12345678%10);
+    System.out.println(12345678/10);
+    System.out.println(12<3);
+    //&&(and) //(or)
+    System.out.println(1<2);
+  }
+}
