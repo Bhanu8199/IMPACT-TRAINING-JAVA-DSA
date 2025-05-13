4d3fe415530b9c51ca7233301f238585faf3c46c
BHANUVARDHAN MEDAPALLI <160517143+Bhanu8199@users.noreply.github.com>
2025-05-13 10:06:53 +0530

    Add files via upload

---
+++ Server Upload Download.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+public class ServerUploadDownload{
+  public static void main (String[] args) {
+    ArrayList<Person> personList=new ArrayList<>();
+    personList.add(new Person("Bhanu","Eluru"));
+    personList.add(new Person("Lavanya","vijayawada"));
+    personList.add(new Person("Prudhvi","Hyderabad"));
+    personList.add(new Person("Phani","Benguluru"));
+    System.out.println(personList);
+    
+    for(Person p:personList){
+      System.out.println(p.name+" "+p.address);
+    }
+  }
+}
+class Person{
+  String name;
+  String address;
+  Person(String name, String address){
+    this.name=name;
+    this.address=address;
+  }
+}
