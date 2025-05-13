35e3f48aad494317fa1395effd0a282e016f25e4
BHANUVARDHAN MEDAPALLI <160517143+Bhanu8199@users.noreply.github.com>
2025-05-13 09:32:11 +0530

    Add files via upload

---
+++ Wrapper Class.java
@@ -0,0 +1,20 @@
+public class WrapperClass {
+  public static void main(String[] args) {
+    
+    
+    // wrapper class
+    // primitive datatypes
+    // byte, short, int, long, float, double, boolean, char
+    
+    int a=12;
+    //wrapper class
+    Integer b=12;
+    String name=b.toString();
+    Byte b1=4;
+    Short sh=45;
+    Long l=456456456456456456L;
+    Float f=45.5f;
+    Double d=45.5;
+    Boolean bl=true;
+  }
+}
