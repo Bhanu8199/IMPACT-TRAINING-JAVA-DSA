81e544ecdc68f30997f709df8bcfcf6b3c56bda4
BHANUVARDHAN MEDAPALLI <160517143+Bhanu8199@users.noreply.github.com>
2025-05-14 09:08:31 +0530

    Add files via upload

---
+++ CREATE A COPY THE  ARRAY.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) {
+    int[] arr = {1, 2, 3, 4, 5, 6, 7};
+
+    // Create a copy of the array
+    int[] arr2 = new int[arr.length];
+    for (int i = 0; i < arr.length; i++) {
+      arr2[i] = arr[i];
+    }
+
+    System.out.println(Arrays.toString(arr2));
+  }
+  // Time-O(n)
+  // Space-O(n)
+}
