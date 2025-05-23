3307556200e5c6badc3e4baaef5fa569976ece78
BHANUVARDHAN MEDAPALLI <160517143+Bhanu8199@users.noreply.github.com>
2025-05-23 09:16:09 +0530

    Add files via upload

---
+++ Queue Example2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class QueueExample2{
+  public static void main (String[] args) {
+    
+    
+    Queue<Integer> queue = new LinkedList<Integer>();
+    queue.add(12);
+    queue.add(15);
+    queue.add(13);
+    queue.add(17);
+    System.out.println(queue);
+    queue.remove();
+    System.out.println(queue);
+    System.out.println(queue.peek());
+  }
+}
+
+
+
